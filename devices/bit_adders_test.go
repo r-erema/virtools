cf6651e2bea3beacd9c56299977888714a3c3471
gutsout <eratut@mail.ru>
2020-04-16 12:19:36 +0300

    Adders

--- devices/bit_adders_test.go
+++ devices/bit_adders_test.go
@@ -7,32 +7,50 @@ import (
 )
 
 func TestEightBitAdderAdd(t *testing.T) {
-	tests := []struct{ input1, input2, wantResult []uint8 }{
-		{
+	tests := []struct {
+		isSubtration               uint8
+		input1, input2, wantResult []uint8
+	}{
+		{0,
 			[]uint8{0, 0, 0, 0, 1, 1, 1, 1},
 			[]uint8{1, 1, 1, 1, 0, 0, 0, 0},
 			[]uint8{0, 1, 1, 1, 1, 1, 1, 1, 1},
 		},
 		{
+			0,
 			[]uint8{0, 0, 0, 0, 0, 0, 0, 0},
 			[]uint8{0, 0, 0, 0, 0, 0, 0, 0},
 			[]uint8{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		},
 		{
+			0,
 			[]uint8{1, 1, 1, 1, 1, 1, 1, 1},
 			[]uint8{1, 1, 1, 1, 1, 1, 1, 1},
 			[]uint8{1, 1, 1, 1, 1, 1, 1, 1, 0},
 		},
 		{
+			0,
 			[]uint8{1, 0, 1, 1, 0, 0, 1, 1},
 			[]uint8{1, 1, 0, 0, 1, 1, 1, 0},
 			[]uint8{1, 1, 0, 0, 0, 0, 0, 0, 1},
+		},
+		{
+			1,
+			[]uint8{1, 1, 1, 1, 1, 1, 0, 1},
+			[]uint8{1, 0, 1, 1, 0, 0, 0, 0},
+			[]uint8{0, 0, 1, 0, 0, 1, 1, 0, 1},
+		},
+		{
+			1,
+			[]uint8{1, 0, 0, 1, 1, 0, 1, 0},
+			[]uint8{0, 1, 1, 0, 0, 0, 1, 1},
+			[]uint8{0, 0, 0, 1, 1, 0, 1, 1, 1},
 		},
 	}
 	a := NewEightBitAdder()
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
-			assert.Equal(t, tt.wantResult, a.Add(0, tt.input1, tt.input2))
+			assert.Equal(t, tt.wantResult, a.Add(tt.isSubtration, tt.input1, tt.input2))
 		})
 	}
 }
